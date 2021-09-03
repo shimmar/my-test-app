40740ac27b8dbe6cf1b10e42fb3ce7115e163296
shimmar <riku2000ff15@gmail.com>
2021-09-04 02:06:14 +0900

    Home画面変更

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template>
-  <hello-world />
+  <div><h2>Version1.0.0</h2></div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
 });
 </script>
